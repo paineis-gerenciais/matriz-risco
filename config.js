a9dd7b46348dc845f87ef6ec4b524b2659410a2e
paineis-gerenciais <84danielfernandes@gmail.com>
2026-07-04 01:45:19 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+// Configuração do dashboard publicado no GitHub Pages.
+// Depois de implantar o Apps Script como Web App, cole a URL /exec abaixo.
+// Exemplo: https://script.google.com/macros/s/AKfycbxxxxxxxxxxxxxxxxxxxx/exec
+window.MATRIZ_RISCO_CONFIG = {
+  APPS_SCRIPT_WEB_APP_URL: "https://script.google.com/macros/s/AKfycby12qoDcSwbqr9KbxpU9jxByLdfzP8weChFLXrQlnOay0YHWOJUJgju4wBmeGqzkUwK/exec"
+};
